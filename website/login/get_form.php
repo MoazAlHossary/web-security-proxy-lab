e12c35d38eced01ad9a3f21398b423d33b9eb5a9
MoazElHossary <moaz_elsayedd@outlook.com>
2025-03-16 23:19:35 +0400

    Create get_form.php
    get login form

---
+++ website/login/get_form.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login via GET</title>
+    <link rel="stylesheet" href="lstyles.css">
+</head>
+<body>
+    <div class="container">
+        <h2>Login (GET Method)</h2>
+        <form action="login_success.php" method="GET">
+        <label for="username">Username:</label>
+            <input type="text" name="username" id="username" placeholder="Enter your username" required>
+
+            <label for="password">Password:</label>
+            <input type="password" name="password" id="password" placeholder="Enter your password" required>
+
+            <input type="submit" value="Login">
+        </form>
+        <a href="../index.html" class="back-home">Back to Home</a>
+        </div>
+</body>
+</html>
